a67e861d30b1179abf237ee47a9ab77030861524
fst1122 <2574520964@qq.com>
2025-09-24 15:36:07 +0800

    配置Markdown支持

--- iblog/blog/models.py
+++ iblog/blog/models.py
@@ -1,3 +1,5 @@
+import mistune
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import is_naive
@@ -75,6 +77,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为Markdown格式")
+    content_html = models.TextField(verbose_name="正文html代码", blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.CASCADE)
@@ -118,6 +121,10 @@ class Post(models.Model):
     def hot_posts(cls):
         return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
 
+
+    def save(self, *args, **kwargs):
+        self.content_html = mistune.markdown(self.content)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
